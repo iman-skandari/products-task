84928dd68b4da4e0126d50f5fa09b46d8a2b1f3b
iman-skandari <iimisk9919@gmail.com>
2024-11-29 00:30:40 +0330

    create new page landing

---
+++ src/page/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ShowProduct from "../../components/ProductList/ProductList"
+
+const Landing = () => {
+  return (
+    <>
+     <div>
+       <ShowProduct />
+     </div>
+    </>
+  )
+}
+
+export default Landing
